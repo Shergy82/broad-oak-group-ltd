9dd67f27268d9e008657c248548f0942d2797830
Phil Shergold <phil.s@broadoakgroup.com>
2026-02-02 22:23:50 +0000

    still the same

--- src/app/admin/control-panel/page.tsx
+++ src/app/admin/control-panel/page.tsx
@@ -1,6 +1,19 @@
 'use client';
 
-import AdminPageContent from "@/components/admin/admin-page-content";
+import { Spinner } from '@/components/shared/spinner';
+import dynamic from 'next/dynamic';
+
+const AdminPageContent = dynamic(
+  () => import('@/components/admin/admin-page-content'),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="flex h-96 w-full items-center justify-center">
+        <Spinner size="lg" />
+      </div>
+    ),
+  }
+);
 
 export default function ControlPanelPage() {
   return <AdminPageContent />;
