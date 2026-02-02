7cc4c91f8cdc9cf4414ebcd236e9932c2f290855
Phil Shergold <phil.s@broadoakgroup.com>
2026-02-02 13:52:17 +0000

    Add team schedule route

---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ShiftScheduleOverview } from '@/components/admin/shift-schedule-overview';
+
+export default function TeamSchedulePage() {
+  return <ShiftScheduleOverview />;
+}
