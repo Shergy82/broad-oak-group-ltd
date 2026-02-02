52a98acfb3fe9a5f6fc46eeb30ebea18e35c52f1
Phil Shergold <phil.s@broadoakgroup.com>
2026-02-02 13:46:03 +0000

    Add site schedule route

---
+++ src/app/site-schedule/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ShiftScheduleOverview } from '@/components/admin/shift-schedule-overview';
+
+export default function SiteSchedulePage() {
+  return <ShiftScheduleOverview />;
+}
