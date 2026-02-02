ee8751d2627cc870805afa0aae44cae2e091264d
Phil Shergold <phil.s@broadoakgroup.com>
2026-02-02 14:59:46 +0000

    Add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Login</h1>
+      <p>This page is now live. Next we’ll wire it to your existing auth UI.</p>
+    </div>
+  );
+}
