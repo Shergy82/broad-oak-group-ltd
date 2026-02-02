e290fed35f1e3eb801e90fe7053e33eae4240b70
Phil Shergold <phil.s@broadoakgroup.com>
2026-02-02 13:52:43 +0000

    Add admin contracts page

---
+++ src/app/admin/contracts/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ContractStatsDashboard } from '@/components/admin/contract-stats-dashboard';
+
+export default function ContractsPage() {
+  return <ContractStatsDashboard />;
+}
