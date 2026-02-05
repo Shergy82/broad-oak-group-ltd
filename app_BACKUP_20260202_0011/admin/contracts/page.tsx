33998203b906f6ce395a09ff4b4e543ed16d7e5e
Phil Shergold <phil.s@broadoakgroup.com>
2026-02-05 14:53:46 +0000

    Remove backup folder from build

--- app_BACKUP_20260202_0011/admin/contracts/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ContractStatsDashboard } from '@/components/admin/contract-stats-dashboard';
-
-export default function ContractsPage() {
-  return <ContractStatsDashboard />;
-}
