3d0481fa46ef1bda65d053ed1ce4a3a2117c3065
Phil Shergold <phil.s@broadoakgroup.com>
2026-03-04 10:36:50 +0000

    when i select either or it doesnt display the import format its just bla

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -129,7 +129,7 @@ const SelectItem = React.forwardRef<
       </SelectPrimitive.ItemIndicator>
     </span>
 
-    {children}
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
   </SelectPrimitive.Item>
 ))
 SelectItem.displayName = SelectPrimitive.Item.displayName
